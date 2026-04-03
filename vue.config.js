832800451e980698b8cd554c8570e0bfe935dde0
fanxyl <fanxyl666@gmail.com>
2026-04-04 00:17:45 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   transpileDependencies: ["vuetify"],
   // GitHub Pages 部署时仓库名为 hsk5，生产构建需要此前缀
   // 本地开发时自动使用 /
-  publicPath: process.env.NODE_ENV === "production" ? "/hsk5/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/HSK5/" : "/",
   outputDir: "dist",
   assetsDir: "static",
   lintOnSave: false,
